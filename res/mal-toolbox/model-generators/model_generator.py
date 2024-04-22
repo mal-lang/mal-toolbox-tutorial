2155b16e20b4ea7838c9d91f3485d1bbfaabeb6a
Andrei Buhaiu <buhaiu@kth.se>
2024-04-22 19:33:14 +0200

    Add model generator code and move lang spec to common folder.

---
+++ res/mal-toolbox/model-generators/model_generator.py
@@ -0,0 +1,91 @@
+import logging
+
+import maltoolbox
+from maltoolbox.language import classes_factory
+from maltoolbox.language import specification
+from maltoolbox import attackgraph
+from maltoolbox import model as malmodel
+from maltoolbox.ingestors import neo4j
+
+logger = logging.getLogger(__name__)
+
+lang_file = 'org.mal-lang.coreLang-1.0.0.mar'
+lang_spec = specification.load_language_specification_from_mar(lang_file)
+specification.save_language_specification_to_json(lang_spec, 'lang_spec.json')
+lang_classes_factory = classes_factory.LanguageClassesFactory(lang_spec)
+lang_classes_factory.create_classes()
+
+model = malmodel.Model('Example Model', lang_spec, lang_classes_factory)
+os_app = lang_classes_factory.ns.Application(name = 'OS App')
+model.add_asset(os_app)
+
+program1 = lang_classes_factory.ns.Application(name = 'Program 1')
+model.add_asset(program1)
+
+appexec_assoc =\
+    lang_classes_factory.ns.AppExecution(
+    hostApp = [os_app],
+    appExecutedApps = [program1])
+model.add_association(appexec_assoc)
+
+sw_vuln = lang_classes_factory.ns.SoftwareVulnerability()
+model.add_asset(sw_vuln)
+
+vuln_assoc =\
+    lang_classes_factory.ns.ApplicationVulnerability_SoftwareVulnerability_Application(
+    application = [os_app],
+    vulnerabilities = [sw_vuln])
+model.add_association(vuln_assoc)
+
+data = lang_classes_factory.ns.Data()
+model.add_asset(data)
+
+app_containment_assoc =\
+    lang_classes_factory.ns.AppContainment(
+    containedData = [data],
+    containingApp = [program1])
+model.add_association(app_containment_assoc)
+
+creds = lang_classes_factory.ns.Credentials()
+model.add_asset(creds)
+
+identity = lang_classes_factory.ns.Identity()
+model.add_asset(identity)
+
+id_creds_assoc =\
+    lang_classes_factory.ns.IdentityCredentials(
+    identities = [identity],
+    credentials = [creds])
+model.add_association(id_creds_assoc)
+
+creds_data_assoc =\
+    lang_classes_factory.ns.InfoContainment(
+    containerData = [data],
+    information = [creds])
+model.add_association(creds_data_assoc)
+
+cr = lang_classes_factory.ns.ConnectionRule()
+model.add_asset(cr)
+
+os_app2 = lang_classes_factory.ns.Application(name = 'Other OS App')
+model.add_asset(os_app2)
+
+appcon_apps_cr_assoc =\
+    lang_classes_factory.ns.ApplicationConnection(
+    applications = [os_app, os_app2],
+    appConnections = [cr])
+model.add_association(appcon_apps_cr_assoc)
+
+attacker = malmodel.AttackerAttachment()
+attacker.entry_points = [(os_app, ['networkConnectUninspected'])]
+
+model.add_attacker(attacker)
+model.save_to_file('example_model.json')
+
+if maltoolbox.neo4j_configs['uri'] != "":
+    neo4j.ingest_model(model,
+        maltoolbox.neo4j_configs['uri'],
+        maltoolbox.neo4j_configs['username'],
+        maltoolbox.neo4j_configs['password'],
+        maltoolbox.neo4j_configs['dbname'],
+        delete=True)
